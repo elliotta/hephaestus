e6ca5b0e19a2884d76194ba180d5b4f0041cb372
Ann Elliott <elliott.222@osu.edu>
2016-12-09 22:10:41 -0500

    Use linearization function from Adafruit's module, and use the new readAll function from my fork of said module

--- MAX31855/max2csv.py
+++ MAX31855/max2csv.py
@@ -38,91 +38,6 @@ def c_to_f(c):
     '''
     return c * 9.0 / 5.0 + 32.0
 
-def corrected_celsius(thermocouple_temp, internal_temp):
-    ''''corrected temperature reading for a K-type thermocouple
-    allowing accurate readings over an extended range
-    http://forums.adafruit.com/viewtopic.php?f=19&t=32086&p=372992#p372992
-    assuming global: Adafruit_MAX31855 thermocouple(CLK, CS, DO);
-
-    Adapted from https://learn.adafruit.com/calibrating-sensors/maxim-31855-linearization
-    '''
-       
-    # MAX31855 thermocouple voltage reading in mV
-    thermocoupleVoltage = (thermocouple_temp - internal_temp) * 0.041276
-       
-    # MAX31855 cold junction voltage reading in mV
-    coldJunctionTemperature = internal_temp
-    coldJunctionVoltage = (-0.176004136860E-01 +
-          0.389212049750E-01  * coldJunctionTemperature +
-          0.185587700320E-04  * pow(coldJunctionTemperature, 2.0) +
-          -0.994575928740E-07 * pow(coldJunctionTemperature, 3.0) +
-          0.318409457190E-09  * pow(coldJunctionTemperature, 4.0) +
-          -0.560728448890E-12 * pow(coldJunctionTemperature, 5.0) +
-          0.560750590590E-15  * pow(coldJunctionTemperature, 6.0) +
-          -0.320207200030E-18 * pow(coldJunctionTemperature, 7.0) +
-          0.971511471520E-22  * pow(coldJunctionTemperature, 8.0) +
-          -0.121047212750E-25 * pow(coldJunctionTemperature, 9.0) +
-          0.118597600000E+00  * exp(-0.118343200000E-03 * 
-                               pow((coldJunctionTemperature-0.126968600000E+03), 2.0) )
-                            )
-                            
-                            
-    # cold junction voltage + thermocouple voltage         
-    voltageSum = thermocoupleVoltage + coldJunctionVoltage
-       
-    # calculate corrected temperature reading based on coefficients for 3 different ranges   
-    if thermocoupleVoltage < 0:
-       b0 = 0.0000000E+00
-       b1 = 2.5173462E+01
-       b2 = -1.1662878E+00
-       b3 = -1.0833638E+00
-       b4 = -8.9773540E-01
-       b5 = -3.7342377E-01
-       b6 = -8.6632643E-02
-       b7 = -1.0450598E-02
-       b8 = -5.1920577E-04
-       b9 = 0.0000000E+00
-       
-    elif thermocoupleVoltage < 20.644:
-       b0 = 0.000000E+00
-       b1 = 2.508355E+01
-       b2 = 7.860106E-02
-       b3 = -2.503131E-01
-       b4 = 8.315270E-02
-       b5 = -1.228034E-02
-       b6 = 9.804036E-04
-       b7 = -4.413030E-05
-       b8 = 1.057734E-06
-       b9 = -1.052755E-08
-       
-    elif thermocoupleVoltage < 54.886:
-        b0 = -1.318058E+02
-        b1 = 4.830222E+01
-        b2 = -1.646031E+00
-        b3 = 5.464731E-02
-        b4 = -9.650715E-04
-        b5 = 8.802193E-06
-        b6 = -3.110810E-08
-        b7 = 0.000000E+00
-        b8 = 0.000000E+00
-        b9 = 0.000000E+00
-       
-    else:
-        # TODO: handle error - out of range
-        return 0
-       
-    return (b0 + 
-        b1 * voltageSum +
-        b2 * pow(voltageSum, 2.0) +
-        b3 * pow(voltageSum, 3.0) +
-        b4 * pow(voltageSum, 4.0) +
-        b5 * pow(voltageSum, 5.0) +
-        b6 * pow(voltageSum, 6.0) +
-        b7 * pow(voltageSum, 7.0) +
-        b8 * pow(voltageSum, 8.0) +
-        b9 * pow(voltageSum, 9.0)
-        )
-
 
 def get_output_file(filename, data_dict, sep):
     """Create or open the output file.
@@ -183,19 +98,21 @@ def main(web_output_file, interval, web_server_port, verbose,
     while True:
         try:
             # Collect the data
-            temps = [sensor.readTempC() for sensor in sensors]
-            internals = [sensor.readInternalC() for sensor in sensors]
-            corrected_temps_f = [c_to_f(corrected_celsius(temp, internal)) for temp, internal in zip(temps, internals)]
+            temps = [sensor.readAll() for sensor in sensors]
+            for t in temps:
+                for k in t.keys():
+                    if k != 'state':
+                        t[k] = c_to_f(t[k])
             now = datetime.datetime.now()
 
             # Stdout output
             if verbose:
-                print(now.isoformat() + ' ' + '; '.join(['%.2f,%.2f,%.2f' % (t, i, c) for t, i, c in zip(temps, internals, corrected_temps_f) ]))
+                print(now.isoformat() + ' ' + '; '.join(['%.2f,%.2f,%.2f' % (t['temp'], t['internal'], t['linearized']) for t in temps]))
 
             # Html output
             # Always overwrite current file
             with open(web_output_file, 'w') as web_file:
-                web_file.write('<html><head><meta http-equiv="refresh" content="1"><title>Current Temps</title></head><body><h1>%s<br><<%s></h1></body></html>' % ('<br>'.join(['%s: %.1f F (%.1f sensor, %.1f internal)' % (name, f, c_to_f(raw_t), c_to_f(raw_i)) for name, f, raw_t, raw_i in zip(SENSOR_NAMES,corrected_temps_f,temps, internals)]), now.isoformat()))
+                web_file.write('<html><head><meta http-equiv="refresh" content="1"><title>Current Temps</title></head><body><h1>%s<br><<%s></h1></body></html>' % ('<br>'.join(['%s: %.1f F (%.1f sensor, %.1f internal)' % (name, t['linearized'], t['temp'], t['internal']) for name, t in zip(SENSOR_NAMES, temps)]), now.isoformat()))
 
             # Log file output
             if not interval_start_time:
@@ -209,9 +126,9 @@ def main(web_output_file, interval, web_server_port, verbose,
                     file_start_time = now
                 # Assemble data dictionary
                 data_dict = OrderedDict([('timestamp', now.strftime('%H:%M:%S')), ('hours', format((now-file_start_time).total_seconds()/3600.0, '06.3f'))])
-                data_dict.update([('%s F' % name, format(temp, '.2f')) for name, temp in zip(SENSOR_NAMES, corrected_temps_f)])
-                data_dict.update([('raw %s F' % name, format(c_to_f(temp), '.2f')) for name, temp in zip(SENSOR_NAMES, temps)])
-                data_dict.update([('internal %s F' % name, format(c_to_f(temp), '.2f')) for name, temp in zip(SENSOR_NAMES, internals)])
+                data_dict.update([('%s F' % name, format(t['linearized'], '.2f')) for name, t in zip(SENSOR_NAMES, temps)])
+                data_dict.update([('raw %s F' % name, format(t['temp'], '.2f')) for name, t in zip(SENSOR_NAMES, temps)])
+                data_dict.update([('internal %s F' % name, format(t['internal'], '.2f')) for name, t in zip(SENSOR_NAMES, temps)])
                 # Write out the data
                 if not output_file or now.date() != current_date:
                     if output_file:
@@ -228,7 +145,7 @@ def main(web_output_file, interval, web_server_port, verbose,
                         short_file_start_time = now
                     # Assemble data dictionary
                     data_dict = OrderedDict([('timestamp', now.strftime('%H:%M:%S')), ('hours', format((now-short_file_start_time).total_seconds()/3600.0, '07.4f'))])
-                    data_dict.update([('%s F' % name, format(temp, '.2f')) for name, temp in zip(SENSOR_NAMES, corrected_temps_f)])
+                    data_dict.update([('%s F' % name, format(t['linearized'], '.2f')) for name, t in zip(SENSOR_NAMES, temps)])
                     # Write out the data
                     if not short_output_file or now.date() != current_date:
                         if short_output_file:
